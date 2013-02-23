7aef713171ffb4dca362d882c849208ee9db9897
Kushagra Gour <chinchang457@gmail.com>
2013-02-24 02:20:57 +0530

    format code indentation to tabs
    fix identation
    
    change indentation to tabs

--- grunt.js
+++ grunt.js
@@ -1,57 +1,57 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: '<json:package.json>',
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
+	// Project configuration.
+	grunt.initConfig({
+		pkg: '<json:package.json>',
+		meta: {
+			banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+				'<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+				'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+				' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+		},
 
-    sass: {
-        dist: {
-            files: {
-                '<%= pkg.name %>.css': 'src/<%= pkg.name %>.scss'
-            }
-        }
-    },
+		sass: {
+			dist: {
+				files: {
+						'<%= pkg.name %>.css': 'src/<%= pkg.name %>.scss'
+				}
+			}
+		},
 
-    mincss: {
-      compress: {
-        files: {
-          '<%= pkg.name %>.min.css': [ '<%= pkg.name %>.css' ]
-        }
-      }
-    },
+		mincss: {
+			compress: {
+				files: {
+					'<%= pkg.name %>.min.css': [ '<%= pkg.name %>.css' ]
+				}
+			}
+		},
 
-    // concat banner to final lib files
-    concat: {
-      lib: {
-        src: ['<banner:meta.banner>', 'hint.css'],
-        dest: 'hint.css'
-      },
-      minlib: {
-        src: ['<banner:meta.banner>', 'hint.min.css'],
-        dest: 'hint.min.css'
-      }
-    },
+		// concat banner to final lib files
+		concat: {
+			lib: {
+				src: ['<banner:meta.banner>', 'hint.css'],
+				dest: 'hint.css'
+			},
+			minlib: {
+				src: ['<banner:meta.banner>', 'hint.min.css'],
+				dest: 'hint.min.css'
+			}
+		},
 
-    watch: {
-      files: 'src/*.scss',
-      tasks: 'default'
-    }
-  });
+		watch: {
+			files: 'src/*.scss',
+			tasks: 'default'
+		}
+	});
 
-  // Dependencies
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-mincss');
-  
-  // Default task.
-  grunt.registerTask('default', 'sass');
-  grunt.registerTask('deploy', 'sass mincss concat');
+	// Dependencies
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-mincss');
+	
+	// Default task.
+	grunt.registerTask('default', 'sass');
+	grunt.registerTask('deploy', 'sass mincss concat');
 
 };
